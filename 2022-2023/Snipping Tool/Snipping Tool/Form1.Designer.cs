67d3905525f313c9791d2ffafd62c7e2b22565a0
Mehmet Uygun <127148730+MehmetUygun0@users.noreply.github.com>
2025-02-25 20:32:08 +0300

    Add files via upload

---
+++ 2022-2023/Snipping Tool/Snipping Tool/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Snipping_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screenshot = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screenshot
+            // 
+            this.screenshot.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.screenshot.Location = new System.Drawing.Point(12, 12);
+            this.screenshot.Name = "screenshot";
+            this.screenshot.Size = new System.Drawing.Size(184, 36);
+            this.screenshot.TabIndex = 0;
+            this.screenshot.Text = "Take a Screenshot";
+            this.screenshot.UseVisualStyleBackColor = true;
+            this.screenshot.Click += new System.EventHandler(this.screenshot_Click);
+            // 
+            // save
+            // 
+            this.save.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.save.Location = new System.Drawing.Point(12, 54);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(184, 33);
+            this.save.TabIndex = 1;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 193);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 25);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Auto Save";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(202, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(364, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.delete.Location = new System.Drawing.Point(12, 140);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(184, 36);
+            this.delete.TabIndex = 5;
+            this.delete.Text = "Delete Last Saved";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(15, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 28);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "screenshot";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(129, 182);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 20);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "JPG";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(129, 208);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 20);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "PNG";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 239);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.screenshot);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Snipping Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button screenshot;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ 2022-2023/Snipping Tool/Snipping Tool/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snipping_Tool
+{
+    public partial class Form1 : Form
+    {
+        Image img;
+        string imgPath;
+        string format="jpg";
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        
+        bool Delete()
+        {
+            if (File.Exists(imgPath))
+            {
+                File.Delete(imgPath);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("File Not Found","Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+        bool Save()
+        {
+            if (img != null)
+            {
+                string fileName = $"{textBox1.Text}_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + $".{format}";
+                imgPath = desktopPath + $@"\{fileName}";
+                img.Save(imgPath); // or img.Save(desktopPath + "\\erdem.jpg");
+                return true;
+            }
+            else MessageBox.Show("Take a Screenshot First", "Warning"); return false;
+        }
+        void Screenshot()
+        {
+            try
+            {
+                Hide();
+                SendKeys.Send("{PRTSC}");
+                Thread.Sleep(1000);
+                img = Clipboard.GetImage();
+                pictureBox1.Image = img;
+                Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Try Again", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                throw;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Select();
+        }
+
+        private void screenshot_Click(object sender, EventArgs e)
+        {
+            Screenshot();
+            if (checkBox1.Checked) Save();
+        }
+
+        private async void save_Click(object sender, EventArgs e)
+        {
+            if (Save()) 
+            {
+                save.BackColor = Color.Green;
+                await Task.Delay(1500);
+                save.BackColor = SystemColors.Control;
+            }
+        }
+
+        private async void delete_Click(object sender, EventArgs e)
+        {
+            if (Delete()) 
+            {
+                delete.BackColor = Color.Red;
+                await Task.Delay(1500);
+                delete.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            save.Enabled= checkBox1.Checked==true?false:true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            format = "jpg";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            format = "png";
+        }
+    }
+}
